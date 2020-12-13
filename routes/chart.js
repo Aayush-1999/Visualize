d3e67da6cb2b3dcc40632072523b92b7e910f40d
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-12-13 13:01:39 +0530

    file upload route completed

--- routes/chart.js
+++ routes/chart.js
@@ -1,15 +1,16 @@
 const express      = require("express"),
       router       = express.Router(),
+      path         = require("path"),
+      utils        = require("../utils/functions"),
       multer       = require("multer"),
-    //   csv          = require('xlsx-parse-json');
-      csv          = require("csvtojson");
+      XLSX         = require('xlsx');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, '/home/aayush_a/Downloads/')
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now())
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
 })
    
@@ -21,71 +22,11 @@ router.post("/uploadFile", upload.single('file'), async(req,res)=>{
         const file = req.file;
         let x = file.originalname.split('.').pop();
 
-        // IF CSV
-        if(x === "csv"){
-            const csvData = await csv().fromFile(file.path);
-
-            let parsedCSV = {
-                columns: [],
-                chartId: "e",
-                dataTypeInfo: {
-                    1: "Date",
-                    2: "Number",
-                    3: "Boolean",
-                    4: "String",
-                    5: "Blank"
-                }
-            };
-
-            for(const property in csvData[0]) {
-                parsedCSV.columns.push({
-                    dataType: 0,
-                    header: property,
-                    values: []
-                });
-            }
-
-            csvData.forEach(el => {
-                parsedCSV.columns.forEach(parsedDataObj => {
-                    let value = el[parsedDataObj.header];
-                    parsedDataObj.values.push(value);
-                    
-                    //SETTING DATATYPE FIELD
-                    if(parsedDataObj.dataType === 0) {
-                        if(/\d/.test(value) === false) {
-                            if(value === "") {
-                                parsedDataObj.dataType = 5;
-                            }
-                            else if((value === "true") || (value === "false")) {
-                                parsedDataObj.dataType = 3;
-                            }
-                            else {
-                                parsedDataObj.dataType = 4;
-                            }
-                        }
-                        else {
-                            if(value.search("-") === -1) {
-                                parsedDataObj.dataType = 2;
-                            }
-                            else {
-                                parsedDataObj.dataType = 1;
-                            }
-                        }
-                    }
-                })
-            });
-
-            parsedCSV.data = {};
-            parsedCSV.data.columns = parsedCSV.columns;
-            parsedCSV.data.numRows = parsedCSV.columns.length;
-            delete parsedCSV.columns;
-
-            res.status(200).json(parsedCSV);
-        }
-        //IF XLSX
-        else {
-           res.json("Excel data")
-        }
+        const workbook = XLSX.readFile(file.path, {cellDates: true});
+        const sheet_name_list = workbook.SheetNames;
+        const tableData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]], { raw: false, defval: ""});
+        const result = utils.generateJSONOutput(tableData)
+        res.status(200).json(result)
     }
     catch(err){
         console.log(err);
