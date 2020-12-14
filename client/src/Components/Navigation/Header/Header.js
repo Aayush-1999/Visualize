f3eb321567eabed703b5d75386a1fe3fc9c2d210
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-12-15 02:43:55 +0530

    Remove unnecessary import

--- client/src/Components/Navigation/Header/Header.js
+++ client/src/Components/Navigation/Header/Header.js
@@ -14,7 +14,6 @@ import Menu from '@material-ui/core/Menu';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 
